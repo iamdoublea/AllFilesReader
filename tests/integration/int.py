801d9bd84ce4df8a668adf9e96b1e61090a3e2e8
iamdoublea <aadityaseal@gmail.com>
2024-05-13 17:39:44 +0530

    test case added

--- tests/integration/int.py
+++ tests/integration/int.py
@@ -1,2 +1,22 @@
-def test_empty():
-    assert True
+#To make sure that the package works well in real world situation, on an overall basis
+#To make sure all the components are working well with each other
+
+import os
+from src.all_files_reader.filereader_crud import DataReader
+
+def test_data_reader_integration():
+    """Tests DataReader with actual file reading (integration test).
+
+    This test assumes sample data files exist in a specific location.
+    You may need to adjust paths or create sample files.
+    """
+
+    # Test CSV
+    data_path = os.path.join('test_data', 'tests\Student_Satisfaction_Survey.csv')
+    reader = DataReader(data_path)
+    data = reader.read()
+    # Perform additional assertions on the CSV data if needed
+
+# Run the tests directly (example)
+if __name__ == '__main__':
+    test_data_reader_integration()
