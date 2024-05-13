88ba7d4a977fb9088e9f13e645216f31e3070465
iamdoublea <aadityaseal@gmail.com>
2024-05-13 16:20:47 +0530

    new updates

---
+++ src/all_files_reader/filereader_crud.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+import scipy.io as sio
+import h5py
+import json
+import csv
+import pickle
+from PIL import Image
+
+
+
+class raeder_operation:
+    def __init__(self):
+        pass
+    
